feb236ad6e8a5ced9960933690c1d08962685fdd
Łukasz Matuszczak <35471793+mlodamrowa@users.noreply.github.com>
2022-11-09 20:14:48 +0100

    main

--- index.php
+++ index.php
@@ -1,3 +1,236 @@
-<?php
-echo "hello";
-?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content=""/>
+    <meta name="keywords" content=""/>
+    <link href="home.css" rel="stylesheet">
+    <title>Strona Glowna</title>
+</head>
+<body>
+<nav>
+    <div id="wrapperNav">
+        <div id="navContainer">
+            <div id="nav-col-1">
+                <a href="#">
+                    LOGO
+                </a>
+            </div>
+            <div id="navLink">
+                <div><a href="#">Rasy</a></div>
+                <div><a href="#">Kategorie</a></div>
+                <div><a href="#">Zadaj Pytanie</a></div>
+            </div>
+        </div>
+        <div id="nav-col-5">
+            <a href="#">
+                <img src="/img/paw.svg" alt="imageLogin">
+            </a>
+        </div>
+    </div>
+</nav>
+
+<main>
+    <section id="main-left-column">
+        <header class="headerMain">
+            <h1>Najnowsze pytania i odpowiedzi</h1>
+        </header>
+        <article>
+            <div class="articleHeader-row-1">
+                <div class="containerAnswer">
+                    <div class="answer">
+                        <p>0</p>
+                        <p>odpowiedzi</p>
+                    </div>
+                </div>
+                <div class="titleArticle">
+                    <a href="#">Jaki kot dla dzieci?</a>
+                </div>
+            </div>
+            <div class="articleHeader-row-2">
+                <div class="viewArticle">
+                    <p>0 wizyt</p>
+                </div>
+                <div class="categories">
+                    <a href="#">
+                        <div class="category">Egzotyczne</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Tanie</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Spokojne</div>
+                    </a>
+                </div>
+                <div class="dateArticle">
+                    21/10/2021
+                </div>
+            </div>
+        </article>
+        <article>
+            <div class="articleHeader-row-1">
+                <div class="containerAnswer">
+                    <div class="answer">
+                        <p>0</p>
+                        <p>odpowiedzi</p>
+                    </div>
+                </div>
+                <div class="titleArticle">
+                    <a href="#">Jaki kot dla dzieci?</a>
+                </div>
+            </div>
+            <div class="articleHeader-row-2">
+                <div class="viewArticle">
+                    <p>0 wizyt</p>
+                </div>
+                <div class="categories">
+                    <a href="#">
+                        <div class="category">Egzotyczne</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Tanie</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Spokojne</div>
+                    </a>
+                </div>
+                <div class="dateArticle">
+                    21/10/2021
+                </div>
+            </div>
+        </article>
+        <article>
+            <div class="articleHeader-row-1">
+                <div class="containerAnswer">
+                    <div class="answer">
+                        <p>0</p>
+                        <p>odpowiedzi</p>
+                    </div>
+                </div>
+                <div class="titleArticle">
+                    <a href="#">Jaki kot dla dzieci?</a>
+                </div>
+            </div>
+            <div class="articleHeader-row-2">
+                <div class="viewArticle">
+                    <p>0 wizyt</p>
+                </div>
+                <div class="categories">
+                    <a href="#">
+                        <div class="category">Egzotyczne</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Tanie</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Spokojne</div>
+                    </a>
+                </div>
+                <div class="dateArticle">
+                    21/10/2021
+                </div>
+            </div>
+        </article>
+        <article>
+            <div class="articleHeader-row-1">
+                <div class="containerAnswer">
+                    <div class="answer">
+                        <p>0</p>
+                        <p>odpowiedzi</p>
+                    </div>
+                </div>
+                <div class="titleArticle">
+                    <a href="#">Jaki kot dla dzieci?</a>
+                </div>
+            </div>
+            <div class="articleHeader-row-2">
+                <div class="viewArticle">
+                    <p>0 wizyt</p>
+                </div>
+                <div class="categories">
+                    <a href="#">
+                        <div class="category">Egzotyczne</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Tanie</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Spokojne</div>
+                    </a>
+                </div>
+                <div class="dateArticle">
+                    21/10/2021
+                </div>
+            </div>
+        </article>
+        <article>
+            <div class="articleHeader-row-1">
+                <div class="containerAnswer">
+                    <div class="answer">
+                        <p>0</p>
+                        <p>odpowiedzi</p>
+                    </div>
+                </div>
+                <div class="titleArticle">
+                    <a href="#">Jaki kot dla dzieci?</a>
+                </div>
+            </div>
+            <div class="articleHeader-row-2">
+                <div class="viewArticle">
+                    <p>0 wizyt</p>
+                </div>
+                <div class="categories">
+                    <a href="#">
+                        <div class="category">Egzotyczne</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Tanie</div>
+                    </a>
+                    <a href="#">
+                        <div class="category">Spokojne</div>
+                    </a>
+                </div>
+                <div class="dateArticle">
+                    21/10/2021
+                </div>
+            </div>
+        </article>
+    </section>
+    <aside>
+        <section id="main-right-column">
+            <div id="webSort" class="headerMain">
+                <h1>
+                    Zmień sortowanie
+                </h1>
+                <img src="/img/sortButton.svg">
+            </div>
+            <div id="asideComment">
+                <h1>Najczesciej komentowane</h1>
+                <hr>
+                <ol>
+                    <li>Czy wasz kot też ...<span>100</span></li>
+                    <li>Czy wasz kot też ...<span>100</span></li>
+                    <li>Czy wasz kot też ...<span>100</span></li>
+                    <li>Czy wasz kot też ...<span>1</span></li>
+                    <li>Czy wasz kot też ...<span>100</span></li>
+                </ol>
+            </div>
+            <div id="asideRegulations">
+                <h1>Regulamin</h1>
+                <hr>
+                <ol>
+                    <li>Wstęp</li>
+                    <li>Zasady korzystania z Serwisu</li>
+                    <li>Zgłoszenia</li>
+                    <li>Konto</li>
+                    <li>Odpowiedzialność</li>
+                    <li>Postanowienia końcowe</li>
+                </ol>
+            </div>
+        </section>
+
+    </aside>
+</main>
+<footer>Copyright © 2022</footer>
+</body>
+</html>
